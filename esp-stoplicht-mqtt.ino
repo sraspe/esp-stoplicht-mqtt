03efe356621f1b6abfa4a102df3461cbdce53262
zeflow <florisladan@gmail.com>
2016-12-27 14:13:41 +0100

    Update esp-stoplicht-mqtt.ino

--- esp-stoplicht-mqtt.ino
+++ esp-stoplicht-mqtt.ino
@@ -1,25 +1,18 @@
 /*
- Basic ESP8266 MQTT example
+Basic MQTT stoplicht
+PIN0 is green
+PIN1 is red
+// Status table
+State - desc - Green - Red
+0 -   off    - Off -Off
+1 - Critical - Off - Blink Continuously 
+2 - Urgent   - Off - ON
+3 - Warning  - On - Blink Continously
+4 - Notify   - On - Blink 3x
+5 - Prepare  - Blink Continuously - Off
+6 - Party    - Blink Continously - Blink Continously
+7 - OK       -On - Off
 
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
-
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
-
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
-
- To install the ESP8266 board, (using Arduino 1.6.4+):
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
 
 */
 
@@ -38,10 +31,13 @@ long lastMsg = 0;
 char msg[50];
 int value = 0;
 String lampStatus;
+int stoplichtStatus;
+
+
 
 void setup() {
-  pinMode(2, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
-  pinMode(0, OUTPUT);
+  pinMode(0, OUTPUT);     // Initialize PIN2 as output
+  pinMode(2, OUTPUT);     // Initialize PIN2 as output
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
@@ -70,7 +66,7 @@ void setup_wifi() {
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
+  Serial.print("Message arrived on topic [");
   Serial.print(topic);
   Serial.print("] ");
   for (int i = 0; i < length; i++) {
